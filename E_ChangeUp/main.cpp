6d0d7ee434287b8a792613aa2af2b187b5f926f1
Mlucas2024 <79484395+Mlucas2024@users.noreply.github.com>
2021-02-23 16:04:10 -0500

    Add files via upload

--- E_ChangeUp/main.cpp
+++ E_ChangeUp/main.cpp
@@ -2,28 +2,28 @@
 
 /*
 ----motorports----
-port desc  port desc
-1  						11 BackRight
-2  						12
-3  						13 frontright
-4 leftspinny 	14
+port desc		  port desc
+1		 				  11 BackRight
+2						  12
+3						  13 frontright
+4	leftspinny	14
 5 rightspinny 15 rightarmupdown
-6  						16
-7  						17
-8 backleft  	18
-9  						19 leftarmupdown
+6						  16
+7						  17
+8 backleft	  18
+9						  19 leftarmupdown
 10 frontleft  20
 
 ----ADI----
-A E
-B F
-C G
-D H
+A				E
+B				F
+C				G
+D				H
 */
 
 void initialize() {
-pros::lcd::initialize();
-pros::lcd::set_text(1, "Hello PROS User!");
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
 }
 
 void disabled() {}
@@ -31,78 +31,116 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
-pros::lcd::set_text(2, "I'm in auto!");
-MotorGroup myLift({15, -19});
-MotorGroup spinny({4, -5});
+	pros::lcd::set_text(2, "I'm in auto!");
+	MotorGroup myLift({15, -19});
+	MotorGroup spinny({4, -5});
+	std::shared_ptr<ChassisController> myBaseDrive =
+		ChassisControllerBuilder()
+				.withMotors({8,10}, {-11,-13})
+				// Green gearset, 4 in wheel diam, 11.5 in whe	el track
+				.withDimensions(AbstractMotor::gearset::green, {{4_in, 13_in}, imev5GreenTPR})
+				.build();
 
-std::shared_ptr<ChassisController> myBaseDrive =
-    ChassisControllerBuilder()
-        .withMotors({8,10}, {-11,-13})
-        // Green gearset, 4 in wheel diam, 11.5 in whe el track
-        .withDimensions(AbstractMotor::gearset::green, {{4_in, 13_in}, imev5GreenTPR})
- .build();
-
- myBaseDrive->setMaxVelocity(150);
-
-//MOVE LIFT UP
- myLift.moveVelocity(200);
- pros::delay(2000);
- myLift.moveVelocity(0);
- 
-//MOVE FORWARD
- myBaseDrive->moveDistance(7_in);
-
-//SPIN BALL OUT
- spinny.moveVelocity(200);
- pros::delay(2000);
- spinny.moveVelocity(0);
-
- //myBaseDrive->turnAngle(90_deg);
-
+myBaseDrive->setMaxVelocity(150);
+//up
+myLift.moveVelocity(200);
+pros::delay(1000);
+myLift.moveVelocity(0);
+//forewoards
+myBaseDrive->moveDistance (6_in);
+//spin ball
+spinny.moveVelocity(200);
+pros::delay(500);
+spinny.moveVelocity(0);
+myBaseDrive->moveDistance (-9_in);
+pros::delay(1000);
+myBaseDrive->turnAngle(200_deg);
+pros::delay(1000);
+myBaseDrive->moveDistance (-7_in);
+pros::delay(1000);
+myLift.moveVelocity(-200);
+pros::delay(1000);
+myLift.moveVelocity(-200);
+pros::delay(1000);
+myLift.moveVelocity(0);
+spinny.moveVelocity(-200);
+pros::delay(1000);
+myBaseDrive->setMaxVelocity(200);
+myBaseDrive->moveDistance (24_in);
+spinny.moveVelocity(0);
+pros::delay(1000);
+myBaseDrive->turnAngle(90_deg);
+pros::delay(1000);
+myLift.moveVelocity(200);
+pros::delay(1000);
+myLift.moveVelocity(0);
+myBaseDrive->moveDistance (24_in);
+spinny.moveVelocity(200);
+pros::delay(1000);
+spinny.moveVelocity(0);
+pros::delay(1000);
+myBaseDrive->moveDistance (-10_in);
+pros::delay(1000);
+myBaseDrive->turnAngle(-150_deg);
+myLift.moveVelocity(-200);
+pros::delay(1000);
+myLift.moveVelocity(0);
+spinny.moveVelocity(-200);
+pros::delay(1000);
+myBaseDrive->moveDistance (36_in);
+spinny.moveVelocity(200);
+pros::delay(1000);
+spinny.moveVelocity(0);
+pros::delay(1000);
+//myBaseDrive->moveDistance (-8_in);
+//myBaseDrive->moveDistance (11_in);
+//pros::delay(1000);
+//spinny.moveVelocity(0);
+//myLift.moveVelocity(0);
 }
 
 void opcontrol() {
 //sets up parts
-Controller myController;
+	Controller myController;
   ControllerButton liftUp(ControllerDigital::L1);
-ControllerButton liftDown(ControllerDigital::R1);
-ControllerButton inTake(ControllerDigital::R2);
-ControllerButton outTake(ControllerDigital::L2);
+	ControllerButton liftDown(ControllerDigital::R1);
+	ControllerButton inTake(ControllerDigital::R2);
+	ControllerButton outTake(ControllerDigital::L2);
 
 MotorGroup myLift({15, -19});
 MotorGroup spinny({4, -5});
 
-std::shared_ptr<ChassisController> myBaseDrive =
+	std::shared_ptr<ChassisController> myBaseDrive =
     ChassisControllerBuilder()
         .withMotors({8,10}, {-11,-13})
-        // Green gearset, 4 in wheel diam, 11.5 in whe el track
+        // Green gearset, 4 in wheel diam, 11.5 in whe	el track
         .withDimensions(AbstractMotor::gearset::green, {{4_in, 13_in}, imev5GreenTPR})
- .build();
+			  .build();
 
-while (true) {
+	while (true) {
 
 
-myBaseDrive->getModel()->arcade(myController.getAnalog(ControllerAnalog::leftY),
-myController.getAnalog(ControllerAnalog::leftX));
+		myBaseDrive->getModel()->arcade(myController.getAnalog(ControllerAnalog::leftY),
+												myController.getAnalog(ControllerAnalog::leftX));
 
 if (liftUp.isPressed()) {
-myLift.moveVelocity(150);
+	myLift.moveVelocity(150);
 } else if (liftDown.isPressed()) {
-myLift.moveVelocity(-150);
+	myLift.moveVelocity(-150);
 }
  else {
-myLift.moveVelocity(0);
+	myLift.moveVelocity(0);
 }
 
 if (inTake.isPressed()) {
-spinny.moveVelocity(-200);
+	spinny.moveVelocity(-200);
 } else if (outTake.isPressed()){
-spinny.moveVelocity(200);
+	spinny.moveVelocity(200);
 }
 else {
-spinny.moveVelocity(0);
+	spinny.moveVelocity(0);
 }
 
-pros::delay(20);
-}
+		pros::delay(20);
+	}
 }
